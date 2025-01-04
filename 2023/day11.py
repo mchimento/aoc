abc57b0318f595e1dea64a18b6dda7270b743119
Mauricio Chimento <jmau.chimento@gmail.com>
2025-01-04 16:33:59 +0100

    day 11 sol part 1

--- 2023/day11.py
+++ 2023/day11.py
@@ -10,109 +10,59 @@ class Day11(Day):
 
     def parse_input(self):
         rows = self.input.process_data_as_rows(0)
-        empty_rows = set()
-        empty_cols = set()
-        for i , row in enumerate(rows):
-            if all(x == '.' for x in row):
-                empty_rows.add(i)
-        ext = 0
-        for ix in empty_rows:
-            rows.insert(ix + ext, '.' * len(rows[0]))
-            ext += 1
-        ext = 0
-        columns = [''.join(column) for column in zip(*rows)]
-        for j , col in enumerate(columns):
-            if all(x == '.' for x in col):
-                empty_cols.add(j)
-        for ix in empty_cols:
-            columns.insert(ix + ext, '.' * len(columns[0]))
-            ext += 1
-        rows = [list(row) for row in zip(*columns)]
-
-        self.grid = Grid(self.input.rows_listed(rows))
+        self.grid = Grid(self.input.rows_listed(self.expand_grid(rows)))
         self.galaxies = self.grid.get_elem_pos('#')
-        id = 0
+        self.grid_map = {}
+        id = 1
         self.galaxies_ids = {}
         for galaxy in self.galaxies:
-            self.grid[galaxy] = id
+            self.grid_map[galaxy] = id
             self.galaxies_ids[id] = galaxy
             id += 1
-        self.grid.print_grid()
+
+    def expand_grid(self, grid):
+        rows_to_expand = [i for i, row in enumerate(grid) if '#' not in row]
+        columns_to_expand = []
+        for col in range(len(grid[0])):
+            if all(row[col] == '.' for row in grid):
+                columns_to_expand.append(col)
+
+        expanded_grid = []
+        for i, row in enumerate(grid):
+            expanded_grid.append(row)
+            if i in rows_to_expand:
+                expanded_grid.append(row)
+
+        final_grid = []
+        for row in expanded_grid:
+            new_row = []
+            for j, char in enumerate(row):
+                new_row.append(char)
+                if j in columns_to_expand:
+                    new_row.append(char)
+            final_grid.append(''.join(new_row))
+
+        return final_grid        
 
     def part1(self):
-        def shortest_paths(start, end, galaxy):
-            """
-            General shortest path algorithm
-            """
-            pq = []
-            parent = {}
-            heapq.heappush(pq, (0, start[0], start[1]))  # (cost, x, y)
-            visited = set()
-            dirs = Directions()
-
-            rows , cols = self.grid.height()-1 , self.grid.width()-1
-
-            while pq:
-                cost, x, y = heapq.heappop(pq)
-
-                if (x, y) in visited:
-                    continue
-                visited.add((x, y))
-
-                if self.grid[x, y] == end:
-                    all_paths = []
-                    reconstruct_paths(x, y, start, parent, all_paths, [(x, y)])
-                    all_paths = list(map(lambda xs: xs[::-1], all_paths))
-                    return cost, all_paths
-
-                for d in dirs.directions:
-                    dx , dy = dirs.coord(d)
-                    nx, ny = x + dx , y + dy
-                    if 0 <= nx <= rows and 0 <= ny <= cols \
-                        and (nx, ny) not in visited \
-                        and self.grid[nx, ny] != dirs.wall:
-                        heapq.heappush(pq, (cost + 1, nx, ny))
-                        if (nx, ny) not in parent:
-                            parent[(nx, ny)] = []
-                        parent[(nx, ny)].append((x, y))
-
-            return None, []
-
-        def reconstruct_paths(x, y, start, parent, all_paths, current_path):
-            if (x, y) == (start[0], start[1]):
-                all_paths.append(list(current_path))
-                return
-            if (x, y) in parent:
-                for px, py in parent[(x, y)]:
-                    current_path.append((px, py))
-                    reconstruct_paths(px, py, start, parent, all_paths, current_path)
-                    current_path.pop()
-
-        def galaxies_shortest_paths(galaxy, visited):
+        def manhattan_distance(start, end):
+            return sum(abs(a - b) for a, b in zip(start, end))
+        
+        def all_distances(galaxy, visited):
             costs = 0
-            id = int(self.grid[galaxy])
+            id = int(self.grid_map[galaxy])
             for key , galaxy2 in self.galaxies_ids.items():
                 if galaxy != galaxy2 and not galaxy2 in visited:
-                    if (id, key) in self.path_cache:
-                        cost = self.path_cache[(galaxy, galaxy2)]
-                    elif (key, id) in self.path_cache:
-                        cost = self.path_cache[(galaxy, galaxy2)]
-                    else:
-                        cost , _ = shortest_paths(galaxy, str(key), galaxy2)
-                        self.path_cache[(id, key)] = cost
-                        self.path_cache[(key, id)] = cost
+                    cost = manhattan_distance(galaxy, galaxy2)
                     costs += cost
             return costs
 
-        self.path_cache = {}
         res = 0
         visited = set()
-        for key , galaxy in self.galaxies_ids.items():
+        for _ , galaxy in self.galaxies_ids.items():
                 visited.add(galaxy)
-                res += galaxies_shortest_paths(galaxy, visited)
-
-        print(self.path_cache)
-
+                res += all_distances(galaxy, visited)
+        
         return res
 
     def part2(self):
