d228a778d2474987e3eaa97fcfa9aae6e67b4d84
Mauricio Chimento <jmau.chimento@gmail.com>
2025-01-04 17:38:18 +0100

    day 11 sol part 2

--- 2023/day11.py
+++ 2023/day11.py
@@ -1,7 +1,5 @@
 from day import Day
 from grid import Grid
-from directions import Directions
-import heapq
 
 class Day11(Day):
 
@@ -9,8 +7,34 @@ class Day11(Day):
         super().__init__()
 
     def parse_input(self):
-        rows = self.input.process_data_as_rows(0)
-        self.grid = Grid(self.input.rows_listed(self.expand_grid(rows)))
+        self.rows = self.input.process_data_as_rows(0)
+
+    def part1(self):
+        def expand_grid(grid):
+            rows_to_expand = [i for i, row in enumerate(grid) if '#' not in row]
+            columns_to_expand = []
+            for col in range(len(grid[0])):
+                if all(row[col] == '.' for row in grid):
+                    columns_to_expand.append(col)
+
+            expanded_grid = []
+            for i, row in enumerate(grid):
+                expanded_grid.append(row)
+                if i in rows_to_expand:
+                    expanded_grid.append(row)
+
+            final_grid = []
+            for row in expanded_grid:
+                new_row = []
+                for j, char in enumerate(row):
+                    new_row.append(char)
+                    if j in columns_to_expand:
+                        new_row.append(char)
+                final_grid.append(''.join(new_row))
+
+            return final_grid
+
+        self.grid = Grid(self.input.rows_listed(expand_grid(self.rows)))
         self.galaxies = self.grid.get_elem_pos('#')
         self.grid_map = {}
         id = 1
@@ -20,34 +44,9 @@ class Day11(Day):
             self.galaxies_ids[id] = galaxy
             id += 1
 
-    def expand_grid(self, grid):
-        rows_to_expand = [i for i, row in enumerate(grid) if '#' not in row]
-        columns_to_expand = []
-        for col in range(len(grid[0])):
-            if all(row[col] == '.' for row in grid):
-                columns_to_expand.append(col)
-
-        expanded_grid = []
-        for i, row in enumerate(grid):
-            expanded_grid.append(row)
-            if i in rows_to_expand:
-                expanded_grid.append(row)
-
-        final_grid = []
-        for row in expanded_grid:
-            new_row = []
-            for j, char in enumerate(row):
-                new_row.append(char)
-                if j in columns_to_expand:
-                    new_row.append(char)
-            final_grid.append(''.join(new_row))
-
-        return final_grid        
-
-    def part1(self):
         def manhattan_distance(start, end):
             return sum(abs(a - b) for a, b in zip(start, end))
-        
+
         def all_distances(galaxy, visited):
             costs = 0
             id = int(self.grid_map[galaxy])
@@ -62,8 +61,60 @@ class Day11(Day):
         for _ , galaxy in self.galaxies_ids.items():
                 visited.add(galaxy)
                 res += all_distances(galaxy, visited)
-        
+
         return res
 
     def part2(self):
-        return super().part2()
+        def expand_grid(grid):
+            self.rows_to_expand = [i for i, row in enumerate(grid) if '#' not in row]
+            for col in range(len(grid[0])):
+                if all(row[col] == '.' for row in grid):
+                    self.columns_to_expand.append(col)
+
+        def manhattan_distance(start, end, expansion):
+            xs , ys = start
+            xe , ye = end
+            expanded = 0
+            x_min = min(xs, xe)
+            x_max = max(xs, xe)
+            y_min = min(ys, ye)
+            y_max = max(ys, ye)
+            for i in self.rows_to_expand:
+                if i >= x_min and i <= x_max:
+                    expanded += 1
+            for j in self.columns_to_expand:
+                if j >= y_min and j <= y_max:
+                    expanded += 1
+
+            return sum(abs(a - b) for a, b in zip(start, end)) + (expanded * expansion)
+
+        def all_distances(galaxy, visited, expansion):
+            costs = 0
+            id = int(self.grid_map[galaxy])
+            for key , galaxy2 in self.galaxies_ids.items():
+                if galaxy != galaxy2 and not galaxy2 in visited:
+                    cost = manhattan_distance(galaxy, galaxy2, expansion)
+                    costs += cost
+            return costs
+
+        res = 0
+        visited = set()
+        self.rows_to_expand = []
+        self.columns_to_expand = []
+        expand_grid(self.rows)
+        self.grid = Grid(self.input.rows_listed(self.rows))
+        self.galaxies = self.grid.get_elem_pos('#')
+        self.grid_map = {}
+        id = 1
+        self.galaxies_ids = {}
+        for galaxy in self.galaxies:
+            self.grid_map[galaxy] = id
+            self.galaxies_ids[id] = galaxy
+            id += 1
+
+        # one expansion are two rows, therefore 9 expanssions are 10 rows, and so on
+        for _ , galaxy in self.galaxies_ids.items():
+                visited.add(galaxy)
+                res += all_distances(galaxy, visited, 999999)
+
+        return res
